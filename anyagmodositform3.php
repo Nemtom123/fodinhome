dc2e516e779d24307529d3630de9831267980b6b
Nemtom123 <dobiasz.tamas@gmail.com>
2017-09-04 07:46:24 +0200

    Add files via upload

---
+++ anyagmodositform3.php
@@ -0,0 +1,35 @@
+<form name="jav" method="post" >
+    <div class="container">
+        <div class="row clearfix">
+            <div class="col-md-12 column">
+                <table class="table table-bordered  table-hover"  id="tab_logic">
+                    <thead class="alert-warning">
+                    <tr >
+                        <th class="text-center">
+                            #
+                        </th>
+                        <th class="text-center">
+                            Megnevezési csoport javítás
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr id='addr0'>
+                        <td>
+                            1
+                        </td>
+                        <td>
+                            <select class="form-control" id="sel1" name="jav" onChange="this.form.submit()">
+                                <?php
+                                while ($sor = $keres->fetch(PDO::FETCH_ASSOC)){
+                                    print "<option value='".$sor['megnevezes_id']."' >".$sor['megnevezes']."</option>";
+                                }
+                                ?>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr id='addr1'></tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
